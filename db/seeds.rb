8be4294d206576c859a370486e77ecd45c7b224f
Aleksandr Kravchenko <a.kravchenko.intek@gmail.com>
2020-02-11 22:22:54 +0100

    solved whitout styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+# TODO: Write a seed to insert 100 posts in the database
+
+puts 'Creating 10 fake posts...'
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph
+  )
+  article.save
+end
+puts 'Finished!'
